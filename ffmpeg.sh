95e1e818871c13a16d30bd86d69afda24a69f08c
shuisheng <2274751790@qq.com>
2023-03-23 20:20:39 +0800

    feat: 优化

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,7 +6,7 @@
 # Email: 2274751790@qq.com
 # FilePath: /billd-live-server/ffmpeg.sh
 # Github: https://github.com/galaxy-s10
-# LastEditTime: 2023-03-23 15:54:19
+# LastEditTime: 2023-03-23 20:20:31
 # LastEditors: shuisheng
 ###
 
@@ -16,4 +16,5 @@
 # 2 代表标准错误
 # 1>/dev/null 把标准输出导入到null设备,也就是消失不见，如果要重定向到某个文件，可以1>1.txt
 # 2>&1 把标准错误也导入到标准输出同样的地方
-ffmpeg -stream_loop -1 -re -i https://resource.hsslive.cn/media/fddm_2.mp4 -c copy -f flv rtmp://localhost/live/fddm_2 1>/dev/null 2>&1 &
+# ffmpeg -stream_loop -1 -re -i https://resource.hsslive.cn/media/fddm_2.mp4 -c copy -f flv rtmp://localhost/live/fddm_2 1>/dev/null 2>&1 &
+ffmpeg -stream_loop -1 -re -i /node/fddm_2.mp4 -c copy -f flv rtmp://localhost/live/fddm_2 1>/dev/null 2>&1 &
