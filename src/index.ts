06a252451e070f87ce72faac33476c657bc30575
shuisheng <2274751790@qq.com>
2023-06-04 23:03:25 +0800

    feat: 优化

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ async function main() {
     handleRedisKeyExpired();
     dockerRunRabbitMQ(true); // docker运行RabbitMQ
     dockerRunSRS(true); // docker运行SRS
-    initFFmpeg(false); // 初始化FFmpeg推流
+    initFFmpeg(true); // 初始化FFmpeg推流
     startSchedule();
     const port = +PROJECT_PORT;
     (await import('./setup')).setupKoa({ port });
