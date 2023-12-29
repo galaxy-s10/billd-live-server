5b3bb84a0c5326ab5e6fd82eace9c76ee940375d
shuisheng <2274751790@qq.com>
2023-12-29 19:30:52 +0800

    feat: sql

--- src/sql/visitorLog.sql.ts
+++ src/sql/visitorLog.sql.ts
@@ -38,23 +38,41 @@ export const getUserVisitRecordSql = (data: {
   userId: number;
   rangTimeStart: string;
   rangTimeEnd: string;
+  orderName?: string;
+  orderBy?: string;
 }) => {
+  let orderStr = '';
+  if (data.orderBy && data.orderName) {
+    orderStr = `ORDER BY ${data.orderName} ${data.orderBy}`;
+  }
   return `
   SELECT
     t3.day AS format_date,
-    COALESCE ( user_id, ${data.userId} ) AS user_id,
-    count( user_id ) AS user_id_nums,
+    t4.username,
+		t4.is_tourist,
+    COUNT( t4.id ) AS user_id_nums,
+    COALESCE ( t4.id, ${data.userId} ) AS user_id,
+    COALESCE ( GROUP_CONCAT( DISTINCT t7.role_id SEPARATOR ', ' ), '' ) AS user_role_id,
+    COALESCE ( GROUP_CONCAT( DISTINCT t8.role_name SEPARATOR ', ' ), '' ) AS user_role_name,
+    COALESCE ( GROUP_CONCAT( DISTINCT t5.user_id SEPARATOR ', ' ), '' ) AS parent_user_id,
+    COALESCE ( GROUP_CONCAT( DISTINCT t6.username SEPARATOR ', ' ), '' ) AS parent_user_username,
     COALESCE ( sum( duration ), 0 ) AS sum_duration,
     COALESCE ( GROUP_CONCAT( DISTINCT live_room_id SEPARATOR ', ' ), '' ) AS live_room_id_str,
     COALESCE ( GROUP_CONCAT( duration SEPARATOR ', ' ), '' ) AS duration_str
   FROM
     ( SELECT user_id, duration, live_room_id, DATE_FORMAT( created_at, '%Y-%m-%d' ) AS format_date FROM visitor_log WHERE deleted_at IS NULL AND user_id = ${data.userId} AND created_at >= '${data.rangTimeStart}' AND created_at <= '${data.rangTimeEnd}' ) AS subquery
     RIGHT JOIN mock_day_data AS t3 ON t3.day = subquery.format_date
+    LEFT JOIN user AS t4 ON t4.id = ${data.userId}
+    LEFT JOIN user_child AS t5 ON t5.child_user_id = ${data.userId}
+    LEFT JOIN user AS t6 ON t6.id = t5.user_id
+    LEFT JOIN user_role AS t7 ON t7.user_id = ${data.userId}
+    LEFT JOIN role AS t8 ON t8.id = t7.role_id
   WHERE
     t3.day >= '${data.rangTimeStart}'
     AND t3.day <= '${data.rangTimeEnd}'
   GROUP BY
   day
+  ${orderStr}
   `;
 };
 
@@ -62,7 +80,13 @@ export const getIpVisitRecordSql = (data: {
   ip: string;
   rangTimeStart: string;
   rangTimeEnd: string;
+  orderName?: string;
+  orderBy?: string;
 }) => {
+  let orderStr = '';
+  if (data.orderBy && data.orderName) {
+    orderStr = `ORDER BY ${data.orderName} ${data.orderBy}`;
+  }
   return `
   SELECT
     t3.day AS format_date,
@@ -79,5 +103,6 @@ export const getIpVisitRecordSql = (data: {
     AND t3.day <= '${data.rangTimeEnd}'
   GROUP BY
   day
+  ${orderStr}
   `;
 };
